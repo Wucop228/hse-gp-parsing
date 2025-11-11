4ce1dcb8cf55d050ef7597cfd7e108df6d8671c5
Roma <you@example.com>
2025-11-12 01:14:51 +0300

    поправил небольшие баги с split proxy

--- parsing_graph_points.py
+++ parsing_graph_points.py
@@ -6,18 +6,20 @@ import requests
 from fake_useragent import UserAgent
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s %(levelname)s: %(message)s",
     datefmt="%H:%M:%S",
+    filename="graph_points.log",
+    encoding="utf-8"
 )
 
 ua = UserAgent()
 logger = logging.getLogger(__name__)
 
+proxies = [None]
 logger.debug("Достаю все proxy из файла .proxies")
 try:
     with open(".proxies") as f:
-        proxies = [None]
         for line in f:
             proxies.append(line)
 except Exception as e:
@@ -25,11 +27,11 @@ except Exception as e:
 else:
     logger.info("Успешно достал все proxy из файла .proxies")
 
+items = []
 logger.debug("Достаю из файла cstable.csv все данные")
 try:
     with open("cstable.csv") as f:
         keys = f.readline().strip().split(',')
-        items = []
         for line in f:
             arr = line.strip().split(',')
             item = {}
@@ -41,8 +43,8 @@ except Exception as e:
 else:
     logger.info("Успешно достал все данные из файла cstable.csv")
 
-def get_graph_points(url, proxy, headers):
-    logger.debug("Пытаюсь запарсить ссылку: url=%s, proxy=%s", url, proxy.split("@")[1])
+def get_graph_points(url, proxy, headers, ip):
+    logger.debug("Пытаюсь запарсить ссылку: url=%s, proxy=%s", url, ip)
     try:
         if proxy != None:
             res = requests.get(url, proxies={"http": proxy, "https": proxy}, headers=headers)
@@ -51,15 +53,15 @@ def get_graph_points(url, proxy, headers):
     except Exception as e:
         logger.error("Ошибка при парсинге: error=%s", e)
     else:
-        logger.info("Успешно запарсил ссылку: url=%s, proxy=%s", url, proxy.split("@")[1])
+        logger.info("Успешно запарсил ссылку: url=%s, proxy=%s", url, ip)
 
-    logger.debug("Проверяю запрос на ошибки: url=%s, proxy=%s", url, proxy.split("@")[1])
+    logger.debug("Проверяю запрос на ошибки: url=%s, proxy=%s", url, ip)
     try:
         res.raise_for_status()
     except requests.exceptions.HTTPError as e:
-        logger.error("Ошибка запроса: error=%s", e)
+        logger.exception("Ответ вернулся с ошибкой: error=%s", e)
     else:
-        logger.info("Запрос успешно выполнен: url=%s, proxy=%s", url, proxy.split("@")[1])
+        logger.info("Запрос успешно выполнен: url=%s, proxy=%s", url, ip)
 
     start = res.text.find("var line1")
     arr_str = res.text[start + len("var line1"):]
@@ -90,18 +92,22 @@ while True:
     success = 0
     failed = 0
     for proxy in proxies:
+        if proxy != None:proxy = proxy.strip()
+        ip = "-"
+        if proxy != None:
+            ip = proxy.split("@")[1]
         for i in range(25):
             url = f"https://steamcommunity.com/market/listings/730/{items[curr]['normalizedname']}"
             try:
-                arr = get_graph_points(url, proxy=proxy, headers={"User-Agent": fake_ua})
+                arr = get_graph_points(url, proxy=proxy, headers={"User-Agent": fake_ua}, ip=ip)
                 arr = [x for x in arr if "2025" in x[0] and ("Oct" in x[0] or "Nov" in x[0])]
                 logger.info("Успешно обработан запрос: curr=%s, url=%s, proxy=%s, item_id=%s, arr_len=%s",
-                            curr, url, proxy.split("@")[1], items[curr]['id'], len(arr))
+                            curr, url, ip, items[curr]['id'], len(arr))
                 lst.append(f"{items[curr]['id']};{items[curr]['normalizedname']};{arr}" + "\n")
                 success += 1
             except Exception as e:
                 logger.error("Ошибка при обработке запроса: curr=%s, url=%s, proxy=%s, item_id=%s, error=%s",
-                             curr, url, proxy.split("@")[1], items[curr]['id'], e)
+                             curr, url, ip, items[curr]['id'], e)
                 failed += 1
                 break
             curr += 1
